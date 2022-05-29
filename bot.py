f2e9f21c75e5296313caaadc55adb3ffde1d0ff6
ttetrafon <ttetrafon@yahoo.gr>
2022-05-29 09:05:19 +0100

    Output description of results alongside degree of success

--- bot.py
+++ bot.py
@@ -2,34 +2,38 @@
 import discord # python -m pip install discord.py
 import random
 import re
+import traceback
 
 props = {}
 
 def load(sep='=', comment_char='#'):
   with open("./credentials.properties", "r") as f:
     for line in f:
-      stripline = line.strip()
-      if stripline and not stripline.startswith(comment_char):
-        key_value = stripline.split(sep)
+      stripLine = line.strip()
+      if stripLine and not stripLine.startswith(comment_char):
+        key_value = stripLine.split(sep)
         key = key_value[0].strip()
         value = sep.join(key_value[1:]).strip().strip('"')
         props[key] = value
 
 def roll(num):
   results = [random.randint(1, 20) for i in range (0, num)]
+  results.sort()
   successes = sum(1 for i in results if i > 10)
-  print([results, successes])
-  return successes
+  return [results, successes]
 
 def parse(ch):
   # format d#1-#2: #1 is the number of rolls, #2 is the required number of successes
   parts = ch.split("-")
   # Get the number of dice
   num = int(parts[0][1:])
-  successes = roll(num)
+  results = roll(num)
+  print(results)
+  description = results[0]
+  successes = results[1]
   if len(parts) > 1:
     successes -= int(parts[1])
-  return successes
+  return [description, successes]
 
 def check(msg):
   print(">>> " + msg)
@@ -37,11 +41,18 @@ def check(msg):
   parts = msg.split(" ")
   # Second part is the initiator of the action
   initiator = parse(parts[1])
+  result = initiator[1]
+  print("result (1):")
+  print(result)
+  description = "[" + ", ".join(map(str, initiator[0])) + "]"
   # Forth part is the opponent
-  opponent = 0
   if len(parts) > 3:
     opponent = parse(parts[3])
-  return initiator - opponent
+    result = result - opponent[1]
+    print("result (2):")
+    print(result)
+    description = description + " vs [" + ", ".join(map(str, opponent[0])) + "]"
+  return str(result) + "\n> " + description
 
 # message = "!check d7"
 # message = "!check d6-2"
@@ -66,11 +77,12 @@ async def on_message(message):
     return
   # If the message pattern is not correct, send back a message to the user with the proper format.
   if message.content.startswith('!check'):
-    msg = "Degree of success: "
+    msg = "Result: "
     try:
       msg += str(check(message.content))
     except:
-      msg = "Expected format: !check d#1-#2 vs d#3-#4\n - #1: initiator's skill, #2 initiator's difficulty\n - #3: opponent's skill, #2 opponent's difficulty\n ! Note that the opponent's stuff are optional!"
+      traceback.print_exc()
+      msg = "Expected format: !check d#1-#2 vs d#3-#4\n - #1: initiator's skill, #2 initiator's difficulty\n - #3: opponent's skill, #2 opponent's difficulty\n ! Note that the opponent's stuff (vs) are optional!"
     await message.channel.send(msg)
 
 client.run(props["clientSecret"])
